983dc73f40ca39efcff2b650de4f5986fcb9bdca
Iqbal Karim <iqbalkarimsiddiqui2015@gmail.com>
2022-12-21 10:43:55 +0300

    Initial Commit

---
+++ ProjectAIRBNB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProjectAIRBNB
+//
+//  Created by Iqbal Karim on 21.12.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
